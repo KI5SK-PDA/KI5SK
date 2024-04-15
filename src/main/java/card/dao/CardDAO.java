64c4bd89c413ba910c3a2e8d98905827bab03efa
kihyun.kwon <kkh3272@naver.com>
2024-04-15 10:56:17 +0900

    feat: add card vo & dao interface

--- src/main/java/card/dao/CardDAO.java
+++ src/main/java/card/dao/CardDAO.java
@@ -1,4 +1,16 @@
 package card.dao;
 
+import card.model.Card;
+import card.model.Purchase;
+import model.vo.Money;
+
+import java.util.List;
+
 public interface CardDAO {
+    void insertCard(Card card);
+    Card findCardByCno(String cno);
+    List<Card> findCardsByUser(String uid);
+    void chargeCard(String cno, int amount);
+    void deleteCard(String cno);
+    Purchase purchase(String cno, String cpw, String store , Money money);//미정
 }
--- src/main/java/card/dao/CardDAOImpl.java
+++ src/main/java/card/dao/CardDAOImpl.java
@@ -1,4 +1,49 @@
 package card.dao;
 
+import card.model.Card;
+import card.model.Purchase;
+import model.vo.Money;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class CardDAOImpl implements CardDAO{
+
+    private HashMap<String, Card> cards;
+    private HashMap<String, Purchase> puchases;
+
+    public CardDAOImpl(){
+        cards = new HashMap<>();
+        puchases = new HashMap<>();
+    }
+
+    @Override
+    public void insertCard(Card card) {
+
+    }
+
+    @Override
+    public Card findCardByCno(String cno) {
+        return null;
+    }
+
+    @Override
+    public List<Card> findCardsByUser(String uid) {
+        return null;
+    }
+
+    @Override
+    public void chargeCard(String cno, int amount) {
+
+    }
+
+    @Override
+    public void deleteCard(String cno) {
+
+    }
+
+    @Override
+    public Purchase purchase(String cno, String cpw, String store, Money money) {
+        return null;
+    }
 }
