657013513d66cb42ebb98e1ddbf4b9691129e82d
Jimin <jimin4124.22@gmail.com>
2024-04-15 18:32:20 +0900

    test: all discounter Test

--- src/test/java/kiosk/model/discounter/TimePercentageDiscounterTest.java
+++ src/test/java/kiosk/model/discounter/TimePercentageDiscounterTest.java
@@ -4,7 +4,6 @@ import common.vo.Money;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Time;
 import java.time.LocalTime;
 
 @DisplayName("시간 비율 할인 테스트")
