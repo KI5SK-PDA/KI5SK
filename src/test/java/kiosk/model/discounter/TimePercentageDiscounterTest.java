1fe7aba544a2f73dd5b07cc5594a9eef7c903fa3
Jimin <jimin4124.22@gmail.com>
2024-04-15 16:20:46 +0900

    feat: discounter-related classes

---
+++ src/test/java/kiosk/model/discounter/TimePercentageDiscounterTest.java
@@ -0,0 +1,13 @@
+package kiosk.model.discounter;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("시간 비율 할인 테스트")
+class TimePercentageDiscounterTest {
+    @DisplayName("생성 테스트")
+    @Test
+    void generateDiscounterTest(){
+        System.out.println("12323");
+    }
+}
