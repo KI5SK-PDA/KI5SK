87ca7c6483b9bb315feca9f439dd8dde19cad2a4
Jimin <jimin4124.22@gmail.com>
2024-04-17 00:06:54 +0900

    feat: OrderServiceImpl

--- src/main/java/kiosk/service/order/dto/req/OrderRequest.java
+++ src/main/java/kiosk/service/order/dto/req/OrderRequest.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class OrderRequest {
     private String cardNumber;
     private String cardPassword;
+    private String store;
     private List<SelectedMenuInfo> menus;
 
 
