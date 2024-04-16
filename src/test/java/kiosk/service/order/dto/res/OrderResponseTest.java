7585253d6a0690d7aab46f9d194aeefbfb8bfe70
Jimin <jimin4124.22@gmail.com>
2024-04-16 11:11:30 +0900

    feat: OrderService DTO

---
+++ src/test/java/kiosk/service/order/dto/res/OrderResponseTest.java
@@ -0,0 +1,17 @@
+package kiosk.service.order.dto.res;
+
+import kiosk.service.order.dto.req.OrderRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+
+@DisplayName("Order Response DTO 테스트")
+class OrderResponseTest {
+    @DisplayName("생성 테스트")
+    @Test
+    void createResponse() {
+
+    }
+}
