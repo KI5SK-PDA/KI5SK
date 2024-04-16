446a4c4bb2becc7d60945eabbe055c9d37432411
Dongwon KIm <dongwon000103@gmail.com>
2024-04-16 12:59:00 +0900

    feat: Store switch to Menu

--- src/main/java/view/store/StoreGridPanel.java
+++ src/main/java/view/store/StoreGridPanel.java
@@ -1,6 +1,7 @@
 package view.store;
 
 import kiosk.controller.KioskController;
+import view.controller.StoreToMenu;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,9 +9,13 @@ import java.awt.*;
 public class StoreGridPanel extends JPanel {
     private final KioskController kioskController = KioskController.newInstance();
 
-    public StoreGridPanel() {
+    public StoreGridPanel(StoreToMenu transition) {
         super.setLayout(new GridLayout(0, 2, 20, 20));
         kioskController.getAllStore().forEach(storeResponse
-            -> add(new StoreSummaryPanel(storeResponse.getId(), storeResponse.getName(), storeResponse.getImagePath())));
+            -> add(new StoreSummaryPanel(
+                storeResponse.getId(),
+            storeResponse.getName(),
+            storeResponse.getImagePath(),
+            new StoreClickListner(storeResponse.getId(), transition))));
     }
 }
--- src/main/java/view/store/StoreSummaryPanel.java
+++ src/main/java/view/store/StoreSummaryPanel.java
@@ -2,22 +2,23 @@ package view.store;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseListener;
 import java.awt.geom.RoundRectangle2D;
 
 public class StoreSummaryPanel extends JPanel {
 
-    public StoreSummaryPanel(String id, String name, String imagePath) {
+    public StoreSummaryPanel(String id, String name, String imagePath, MouseListener storeClickListener) {
         Box box = Box.createVerticalBox();
         box.add(Box.createVerticalGlue());
 
         setPreferredSize(new Dimension(300, 300));
         JLabel imageLabel = new StoreImage(new ImageIcon(getClass().getClassLoader().getResource(imagePath)));
-        imageLabel.addMouseListener(new StoreClickListner(id));
+        imageLabel.addMouseListener(storeClickListener);
         box.add(imageLabel);
 
         JLabel nameLabel = new JLabel(name);
         nameLabel.setFont(new Font("Arial", Font.BOLD, 20));
-        nameLabel.addMouseListener(new StoreClickListner(id));
+        nameLabel.addMouseListener(storeClickListener);
         box.add(nameLabel);
 
         add(box);
@@ -25,7 +26,6 @@ public class StoreSummaryPanel extends JPanel {
 
     private static class StoreImage extends JLabel {
         StoreImage(ImageIcon imageIcon) {
-
             this.setIcon(new ImageIcon(imageIcon.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH)));
         }
 
