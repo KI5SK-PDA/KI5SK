ce9424ee0d4e7afed5c59ec4e5e1fade29c4f64d
Jimin <jimin4124.22@gmail.com>
2024-04-17 00:06:54 +0900

    feat: OrderServiceImpl

--- src/main/java/kiosk/service/order/dto/res/OrderResponse.java
+++ src/main/java/kiosk/service/order/dto/res/OrderResponse.java
@@ -6,8 +6,10 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
+@Setter
 public class OrderResponse {
     private boolean success;
     private String message;
-    private int amountOfPurchase;
+    private int amountOfPurchase; // 구매금액(할인 적용)
+    private int amountOfOrder; // 주문금액(할인 미적용)
 }
--- src/test/java/card/service/CardServiceImplTest.java
+++ src/test/java/card/service/CardServiceImplTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("카드 서비스 테스트")
 class CardServiceImplTest {
 
-    CardServiceImpl cardService = new CardServiceImpl();
+    CardServiceImpl cardService = CardServiceImpl.getInstance();
 
     @DisplayName("새로운 카드번호 발급")
     @Test
