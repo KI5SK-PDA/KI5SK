9a5a45fcb9abd09d0ddcdfc2dad62db2dbf038e8
kihyun.kwon <kkh3272@naver.com>
2024-04-16 17:03:39 +0900

    fix: user .idea 폴더 관련하여 필요없는 것 삭제

--- src/main/java/KI5SKApp.java
+++ src/main/java/KI5SKApp.java
@@ -2,9 +2,9 @@ import user.controller.MainController;
 
 public class KI5SKApp {
     public static void main(String[] args) {
-        /*
+
         MainController mainController = new MainController();
         mainController.start();
-        */
+
     }
 }
