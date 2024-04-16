4dfd4da9895792a447fc2d3436e690579e49913c
kihyun.kwon <kkh3272@naver.com>
2024-04-17 00:15:18 +0900

    temp

--- src/main/java/view/menu/ShoppingBasketPanel.java
+++ src/main/java/view/menu/ShoppingBasketPanel.java
@@ -1,6 +1,8 @@
 package view.menu;
 
 import kiosk.service.menu.dto.res.MenuInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,17 +10,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public class ShoppingBasketPanel extends JPanel {
+public class ShoppingBasketPanel extends JPanel implements ActionListener {
 
     private List<MenuInfo> menus;
 
+
     JPanel mainPanel = new JPanel(); // 스크롤 가능한 메인 패널
     JPanel basketItemListPanel = new JPanel(); // 상품 목록 패널
+    JLabel totalAmountLabel = new JLabel("총 결제금액: 0원"); //실제 결제금액 계산하도록
+    JButton purchaseButton = new JButton("결제");
+
+    private int sumPrice;
+
+    private List<SelectedMenu> selectedMenus;
 
     public ShoppingBasketPanel() {
-
         menus = new ArrayList<>();
+
+        selectedMenus = new ArrayList<>();
+        sumPrice = 0;
 
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         basketItemListPanel.setLayout(new BoxLayout(basketItemListPanel, BoxLayout.Y_AXIS));
@@ -28,13 +40,10 @@ public class ShoppingBasketPanel extends JPanel {
         JScrollPane scrollPane = new JScrollPane(basketItemListPanel);
         scrollPane.getVerticalScrollBar().setUnitIncrement(16);
         mainPanel.add(scrollPane);
-
-        JLabel totalAmountLabel = new JLabel("총 결제금액: 0원"); //실제 결제금액 계산하도록
         totalAmountLabel.setFont(new Font("Arial", Font.BOLD, 20));
         mainPanel.add(totalAmountLabel);
 
         JPanel purchasePanel = new JPanel();
-        JButton purchaseButton = new JButton("결제");
         purchaseButton.setFont(new Font("Arial", Font.BOLD, 30));
         purchaseButton.setPreferredSize(new Dimension(300, 40));
         purchasePanel.add(purchaseButton);
@@ -45,8 +54,34 @@ public class ShoppingBasketPanel extends JPanel {
 
     public void addSelectedMenus(MenuInfo menu){
         menus.add(menu);
+        selectedMenus.add(SelectedMenu.builder()
+                        .name(menu.getName())
+                        .id(menu.getId())
+                        .discountPrice(menu.getDiscountPrice())
+                        .originalPrice(menu.getOriginalPrice())
+                .build());
+
+        addSumPrice(menu);
         viewSelectedMenus(menu);
         updateUI();
+    }
+
+    public void addSumPrice(MenuInfo menu){
+        if(!menu.getDiscountPrice().isEmpty()){
+            sumPrice += menu.getDiscountPrice().get();
+        } else {
+            sumPrice += menu.getOriginalPrice();
+        }
+        totalAmountLabel.setText("총 결제금액: "+sumPrice+"원");
+    }
+
+    public void subSumPrice(MenuInfo menu, int count){
+        if(!menu.getDiscountPrice().isEmpty()){
+            sumPrice -= menu.getDiscountPrice().get() * count;
+        } else {
+            sumPrice -= menu.getOriginalPrice() * count;
+        }
+        totalAmountLabel.setText("총 결제금액: "+sumPrice+"원");
     }
 
     public void viewSelectedMenus(MenuInfo menu){
@@ -60,16 +95,30 @@ public class ShoppingBasketPanel extends JPanel {
         nameLabel.setFont(new Font("Serif", Font.BOLD, 20)); //실제로 추가된 메뉴만 구현하도록.
 
         JLabel priceLabel = new JLabel("가격: "+menu.getOriginalPrice()+"원");
-        JLabel discountPriceLabel = new JLabel("할인 가격: "+menu.getDiscountPrice()+"원"); //가격을 계산하도록.
+        String discountPrice = menu.getDiscountPrice().isEmpty() ? "대상 아님" : menu.getDiscountPrice().get()+"원";
+        JLabel discountPriceLabel = new JLabel("할인 가격: "+discountPrice); //가격을 계산하도록.
 
-        SpinnerModel quantityModel = new SpinnerNumberModel(1, 1, 100, 1);
+        SpinnerModel quantityModel = new SpinnerNumberModel(1, 1, 100, -1);
         JSpinner quantitySpinner = new JSpinner(quantityModel);
+
+        quantitySpinner.addChangeListener(e -> {
+            int oldValue = (int) quantityModel.getPreviousValue();
+            int newValue = (int) quantityModel.getValue();
+//            int nextValue = (int) quantityModel.getNextValue();
+            System.out.println("oldValue : "+oldValue);
+            System.out.println("newValue : "+newValue);
+//            System.out.println("nextValue : "+nextValue);
+//            if(oldValue < newValue){
+//                addSumPrice(menu);
+//            }
+        });
 
         JButton cancelButton = new JButton("삭제");
         cancelButton.addActionListener(e -> {
             basketItemListPanel.remove(itemPanel);
             basketItemListPanel.revalidate();
             basketItemListPanel.repaint();
+            subSumPrice(menu, (int)quantityModel.getValue());
         });
 
         firstLinePanel.add(nameLabel);
@@ -106,5 +155,23 @@ public class ShoppingBasketPanel extends JPanel {
         itemPanel.add(optionsPanel);
 
         basketItemListPanel.add(itemPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(purchaseButton.equals(e.getSource())){
+            // 결제 창 구현
+
+        }
+    }
+
+    @Builder
+    @AllArgsConstructor
+    private class SelectedMenu {
+        String id;
+        int originalPrice;
+        Optional<Integer> discountPrice;
+        int quantity;
+        String name;
     }
 }
