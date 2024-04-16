8905790878405dad00afb6fb5e35ae76ac7acc7b
kihyun.kwon <kkh3272@naver.com>
2024-04-17 02:41:25 +0900

    feat: 결제 부분 카드 비번 입력 구현

--- src/main/java/view/purchase/PurchasePanel.java
+++ src/main/java/view/purchase/PurchasePanel.java
@@ -13,7 +13,8 @@ import java.util.List;
 public class PurchasePanel extends JPanel implements ActionListener {
 
     JLabel laTitle = new JLabel("카드를 선택해주세요");
-    JLabel laDescription = new JLabel("* 결제하길 원하는 카드 선택하면 바로 결제됩니다.");
+    JLabel laDescription = new JLabel("* 비밀번호를 입력하고 결제를 원하는 카드를 선택하면 바로 결제됩니다.");
+    JPasswordField pfPassword = new JPasswordField();
     CardController cardController;
     List<Card> cards;
     JPanel cardPanelLayout = new JPanel();
@@ -32,6 +33,10 @@ public class PurchasePanel extends JPanel implements ActionListener {
         laDescription.setFont(new Font("Arial", Font.BOLD, 16));
         laDescription.setForeground(Color.lightGray);
         cardPanelLayout.add(laDescription, BorderLayout.NORTH);
+
+        pfPassword.setFont(new Font("Arial", Font.BOLD, 32));
+        pfPassword.setPreferredSize(new Dimension(450, 60));
+        cardPanelLayout.add(pfPassword, BorderLayout.NORTH);
 
         getCards();
         viewCardPanel();
@@ -56,9 +61,12 @@ public class PurchasePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        for(JButton jButton: cardsPanel){
-            if(jButton.equals(e.getSource())){
+        for(int i=0; i<cards.size(); i++){
+            if(cardsPanel.get(i).equals(e.getSource())){
+                String cardNumber = cards.get(i).getCno();
+                String cardPassword = new String(pfPassword.getPassword());
                 // TODO 결제 진행코드
+                pfPassword.setText("");
             }
         }
     }
