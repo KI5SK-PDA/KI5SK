da43b6efe53bba011efc98a8f310c91dd767dcdb
seohee99 <imshee99@gmail.com>
2024-04-17 13:09:21 +0900

    feat: add init data

--- src/main/java/KI5SKApp.java
+++ src/main/java/KI5SKApp.java
@@ -20,8 +20,8 @@ public class KI5SKApp {
     }
 
     public static void initStore() {
-        List<String> storeNames = Arrays.asList("📍 쿵후 마라탕 성수점", "📍 BBQ 성수3점", "📍 제주국수");
-        List<String> imagePaths = Arrays.asList("store/maratang.png", "store/bbq.jpeg", "store/jeju.jpg");
+        List<String> storeNames = Arrays.asList("📍 쿵후 마라탕 성수점", "📍 BBQ 성수3점", "📍 제주국수", "📍 산들김밥", "📍 엉터리생고기", "📍 코끼리베이글", "📍 자연도 소금빵", "📍 써브웨이");
+        List<String> imagePaths = Arrays.asList("store/maratang.png", "store/bbq.jpeg", "store/jeju.jpg", "store/sandle.jpg","store/ungseang.jpeg","store/bagle.jpeg", "store/bbang.jpeg", "store/subway.jpeg");
 
         for (int j=0;j< storeNames.size();j++) {
             adminKioskController.launchStore(LaunchStoreRequest.builder()
@@ -35,60 +35,6 @@ public class KI5SKApp {
     public static void initMenu() {
         initMaratangMenu();
         initBBQMenu();
-    }
-
-    private static void initBBQMenu(){
-        // BBQ
-        String singleCategoryId = adminKioskController.insertCategory("001", "단품");
-        String setCategoryId = adminKioskController.insertCategory("001", "세트");
-        String sideCategoryId = adminKioskController.insertCategory("001", "사이드");
-        String beverageId = adminKioskController.insertCategory("001", "음료");
-
-        String newBBQMenuId = adminKioskController.insertAlwaysCashDiscountMenu(singleCategoryId, InsertMenuRequest.builder()
-                .name("🏷️ BBQ(⭐신메뉴 이벤트⭐)")
-                .originalPrice(20000)
-                .build());
-
-        String newBBQMenusId = adminKioskController.insertOptionGroup(newBBQMenuId, "신메뉴");
-        adminKioskController.insertOptionMenu(newBBQMenuId, newBBQMenusId, "🔖 바사칸윙(23,000 -> 22,000)",2000);
-        adminKioskController.insertOptionMenu(newBBQMenuId, newBBQMenuId, "🔖 BBQ 양념치킨(22,000 -> 21,500",500);
-        adminKioskController.insertOptionMenu(newBBQMenuId,newBBQMenusId, "🔖  황올 반 + BBQ 양념 반(22,000 -> 21000)",1000);
-
-        String goldOliveChicken = adminKioskController.insertNoDiscountMenu(sideCategoryId, InsertMenuRequest.builder()
-                .name("🏷️ 황금 올리브 치킨")
-                .originalPrice(20000)
-                .build());
-
-        String goldOliveChickenleg = adminKioskController.insertNoDiscountMenu(sideCategoryId, InsertMenuRequest.builder()
-                .name("🏷️ 황금 올리브 치킨(닭다리)")
-                .originalPrice(21000)
-                .build());
-
-        String basakGalic = adminKioskController.insertNoDiscountMenu(sideCategoryId, InsertMenuRequest.builder()
-                .name("🏷️ 바삭갈릭")
-                .originalPrice(21000)
-                .build());
-
-        String danzzanGalic = adminKioskController.insertNoDiscountMenu(sideCategoryId, InsertMenuRequest.builder()
-                .name("🏷️ 단짠갈릭")
-                .originalPrice(23000)
-                .build());
-
-        String TwolegSet = adminKioskController.insertNoDiscountMenu(setCategoryId, InsertMenuRequest.builder()
-                .name("🏷️ 다리가 두개 더 SET")
-                .originalPrice(26000)
-                .build());
-
-        String TwoChickenSet = adminKioskController.insertNoDiscountMenu(setCategoryId, InsertMenuRequest.builder()
-                .name("🏷️ 두마리 치킨 SET")
-                .originalPrice(40500)
-                .build());
-
-        String OneHalfSet = adminKioskController.insertNoDiscountMenu(setCategoryId, InsertMenuRequest.builder()
-                .name("🏷️ 한마리 반 치킨 SET")
-                .originalPrice(33500)
-                .build());
-
     }
 
     private static void initMaratangMenu() {
@@ -166,6 +112,60 @@ public class KI5SKApp {
             .name("카스")
             .originalPrice(5000)
             .build());
+    }
+
+    private static void initBBQMenu(){
+        // BBQ
+        String singleCategoryId = adminKioskController.insertCategory("001", "단품");
+        String setCategoryId = adminKioskController.insertCategory("001", "세트");
+        String sideCategoryId = adminKioskController.insertCategory("001", "사이드");
+        String beverageId = adminKioskController.insertCategory("001", "음료");
+
+        String newBBQMenuId = adminKioskController.insertAlwaysCashDiscountMenu(singleCategoryId, InsertMenuRequest.builder()
+                .name("🏷️ BBQ(⭐신메뉴 이벤트⭐)")
+                .originalPrice(20000)
+                .build());
+
+        String newBBQMenusId = adminKioskController.insertOptionGroup(newBBQMenuId, "신메뉴");
+        adminKioskController.insertOptionMenu(newBBQMenuId, newBBQMenusId, "🔖 바사칸윙(23,000 -> 22,000)",2000);
+        adminKioskController.insertOptionMenu(newBBQMenuId, newBBQMenuId, "🔖 BBQ 양념치킨(22,000 -> 21,500",500);
+        adminKioskController.insertOptionMenu(newBBQMenuId,newBBQMenusId, "🔖  황올 반 + BBQ 양념 반(22,000 -> 21000)",1000);
+
+        String goldOliveChicken = adminKioskController.insertNoDiscountMenu(sideCategoryId, InsertMenuRequest.builder()
+                .name("🏷️ 황금 올리브 치킨")
+                .originalPrice(20000)
+                .build());
+
+        String goldOliveChickenleg = adminKioskController.insertNoDiscountMenu(sideCategoryId, InsertMenuRequest.builder()
+                .name("🏷️ 황금 올리브 치킨(닭다리)")
+                .originalPrice(21000)
+                .build());
+
+        String basakGalic = adminKioskController.insertNoDiscountMenu(sideCategoryId, InsertMenuRequest.builder()
+                .name("🏷️ 바삭갈릭")
+                .originalPrice(21000)
+                .build());
+
+        String danzzanGalic = adminKioskController.insertNoDiscountMenu(sideCategoryId, InsertMenuRequest.builder()
+                .name("🏷️ 단짠갈릭")
+                .originalPrice(23000)
+                .build());
+
+        String TwolegSet = adminKioskController.insertNoDiscountMenu(setCategoryId, InsertMenuRequest.builder()
+                .name("🏷️ 다리가 두개 더 SET")
+                .originalPrice(26000)
+                .build());
+
+        String TwoChickenSet = adminKioskController.insertNoDiscountMenu(setCategoryId, InsertMenuRequest.builder()
+                .name("🏷️ 두마리 치킨 SET")
+                .originalPrice(40500)
+                .build());
+
+        String OneHalfSet = adminKioskController.insertNoDiscountMenu(setCategoryId, InsertMenuRequest.builder()
+                .name("🏷️ 한마리 반 치킨 SET")
+                .originalPrice(33500)
+                .build());
+
     }
 
 
